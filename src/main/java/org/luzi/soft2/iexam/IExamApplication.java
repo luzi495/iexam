cf7f93775bc88bbb4749b41c69f02591768df249
Administrator <luzi495@163.com>
2016-11-06 16:24:23 +0800

    添加测试用例配置和产品配置；添加启动类。
    Change-Id: Ifdd9ae9c76665c4bc40d778738208c3493bba5a1

---
+++ src/main/java/org/luzi/soft2/iexam/IExamApplication.java
@@ -0,0 +1,11 @@
+package org.luzi.soft2.iexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IExamApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(IExamApplication.class, args);
+	}
+}
